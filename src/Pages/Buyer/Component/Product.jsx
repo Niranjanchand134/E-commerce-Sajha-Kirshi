e6218dc3e5ccf5569870d45b1ef49d842020e44a
niranjan chand <niranjachand134@gmail.com>
2025-06-26 07:47:34 +0545

    farmer header and side bar part- user name updated

--- src/Pages/Buyer/Component/Product.jsx
+++ src/Pages/Buyer/Component/Product.jsx
@@ -53,46 +53,48 @@ const Product = () => {
           </Link>
         </div>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 md:px-60">
-          {products.map((product) => (
-            <Link
-              key={product.id}
-              to={`/shop-detail/${product.id}`}
-              className="text-center rounded hover:shadow-lg transition-shadow"
-            >
-              <div className="text-center rounded">
-                <img
-                  src={
-                    product.imagePaths || "/assets/BuyersImg/Products/Onion.png"
-                  } // Fallback image if product.image doesn't exist
-                  alt={product.name}
-                />
-                <div className="flex justify-between">
-                  <div className="p-2">
-                    <h5>{product.name}</h5>
-                    <p className="text-green-500 text-lg">
-                      Rs {product.price || "00.00"}
-                    </p>
-                  </div>
-                  <div className="text-right">
-                    <p className="mt-1 ">
-                      {farmerDetails[product.user.id]?.farmName || "Farm Name"}
-                    </p>
-                    <div className="flex">
-                      <p className="text-[10px]">
-                        {farmerDetails[product.user.id]?.district || "Location"}
-                      </p>
-                      <p className="text-[10px]">
-                        {(farmerDetails[product.user.id]?.district &&
-                          farmerDetails[product.user.id]?.municipality) ||
-                          "Location"}
+        <div className="flex justify-center items-center min-h-screen">
+          <div className="w-full md:w-3/4 bg-white rounded-lg ">
+            <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
+            {products.map((product) => (
+              <Link
+                // key={product.id}
+                to={`/shopdetail`}
+                className="rounded text-black no-underline transition-shadow duration-300 hover:shadow-md"
+              >
+                  <img
+                    src={
+                      product.imagePaths || "/assets/BuyersImg/Products/Onion.png"
+                    } // Fallback image if product.image doesn't exist
+                    alt={product.name}
+                  />
+                  <div className="flex justify-between">
+                    <div className="p-2">
+                      <h5>{product.name}</h5>
+                      <p className="text-green-500 text-lg">
+                        Rs {product.price || "00.00"}
                       </p>
                     </div>
+                    <div className="text-right p-2">
+                      <p>
+                        {farmerDetails[product.user.id]?.farmName || "Farm Name"}
+                      </p>
+                      <div className="flex gap-1">
+                        <p>
+                          {farmerDetails[product.user.id]?.district || "Location"}
+                        </p>
+                        <p>
+                          {(farmerDetails[product.user.id]?.district &&
+                            farmerDetails[product.user.id]?.municipality) ||
+                            "Location"}
+                        </p>
+                      </div>
+                    </div>
                   </div>
-                </div>
-              </div>
-            </Link>
-          ))}
+              </Link>
+            ))}
+          </div>
+        </div>
         </div>
       </div>
     </>
